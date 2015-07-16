ad847668fbb7300159c02fc91f7780ceefe459ac
Daniel Pancu <d.pancu@levi9.com>
2015-07-16 17:29:33 +0300

    LEVEMM - 8 : Account and Role model created

---
+++ employee-manager-model/src/main/java/org/employee_manager/model/Role.java
@@ -0,0 +1,65 @@
+package org.employee_manager.model;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ROLE")
+public class Role implements Serializable {
+
+	@Id
+	@Column(name="ROLE_ID", nullable=false)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_GEN_ROLE")
+	@SequenceGenerator(name = "SEQ_GEN_ROLE", sequenceName = "role_id_sequence", allocationSize = 10)
+	private long id;
+	
+	@ManyToOne
+    @JoinColumn(name="ACCOUNT_ID", nullable=false)
+    private Account accountId;
+	
+	@Column(name="NAME")
+	private String name;
+	
+	@Column(name="DESCRIPTION")
+	private String description;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public Account getAccountId() {
+		return accountId;
+	}
+
+	public void setAccountId(Account accountId) {
+		this.accountId = accountId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
