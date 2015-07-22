ae0d70c67e0d12578426ce8e650bd661df614f42
Diaconu Stefan <diaconustefandaniel@gmail.com>
2015-07-22 18:20:39 +0300

    added some functionality to SkillRestService

--- employee-manager-services/src/main/java/org/employee_manager/services/rest/SkillRestService.java
+++ employee-manager-services/src/main/java/org/employee_manager/services/rest/SkillRestService.java
@@ -1,10 +1,36 @@
 package org.employee_manager.services.rest;
 
-import javax.ws.rs.Path;
+import java.util.List;
 
-@Path("skill")
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.employee_manager.model.Skill;
+import org.employee_manager.services.RoleService;
+import org.employee_manager.services.SkillService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+@Path("/skill")
 public class SkillRestService {
 
+	@Autowired
+	private SkillService skillService;
+	
+	@Autowired
+	private RoleService roleService;
+	
+	@GET
+	@Path("")
+	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+	public List<Skill> getAllSkills()
+	{
+		return skillService.findAllSkills();
+		
+	}
 	
 	/*@GET
 	@Path("{}")
