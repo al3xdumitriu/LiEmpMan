39ecdd9ffe4612bfc5cdb83854aa960f5ddf8dea
SStefanVlad <s.sinescu@levi9.com>
2015-07-22 18:06:59 +0300

    added rest class

---
+++ employee-manager-services/src/main/java/org/employee_manager/services/rest/EmployeeRestService.java
@@ -0,0 +1,56 @@
+package org.employee_manager.services.rest;
+
+import java.util.Date;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.employee_manager.model.Employee;
+import org.employee_manager.model.Event;
+import org.employee_manager.services.EmployeeService;
+import org.employee_manager.services.repositories.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+@Path("/employees")
+public class EmployeeRestService {
+
+	@Autowired
+	private EmployeeService employeeService;
+
+	@Autowired
+	private EmployeeRepository employeeRepository;
+
+	@POST
+	@Path("")
+	@Produces("application/json")
+	public Response saveEmployee() {
+
+		int status = 200;
+		Employee emp1 = new Employee();
+		emp1.setEmail("adsas@yahoo.ro");
+		emp1.setAvailableHours(5);
+		emp1.setExperienceLevel("2 ani jumate");
+		emp1.setJobTitle("developer");
+		emp1.setName("John");
+		emp1.setPhone("0743991341");
+		
+
+		try {
+			employeeService.save(emp1);
+
+			status = 200;
+
+		} catch (Exception e) {
+			status = 404;
+			e.getStackTrace();
+		}
+		Response res = Response.status(status).entity(emp1).build();
+		return res;
+	}
+}
