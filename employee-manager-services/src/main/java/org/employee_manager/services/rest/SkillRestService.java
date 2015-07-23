90c5fff9c5ceb3c972c78d3d648eccaee8cab5d8
SStefanVlad <s.sinescu@levi9.com>
2015-07-23 09:07:41 +0300

    pom jersey dependencies

--- employee-manager-services/src/main/java/org/employee_manager/services/rest/SkillRestService.java
+++ employee-manager-services/src/main/java/org/employee_manager/services/rest/SkillRestService.java
@@ -2,7 +2,34 @@ package org.employee_manager.services.rest;
 
 import javax.ws.rs.Path;
 
-@Path("skill")
+
 public class SkillRestService {	
+	
+	/*@GET
+	@Path("{}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+	public SolrDocumentList querySolr() throws SolrServerException, IOException
+	{
+		SolrClient solr=SolrClientApp.instantiateSolr();
+		
+		
+		SolrQuery parameters = new SolrQuery();
+		
+		String mQueryString="last_name:Wixon";
+		parameters.set("q", mQueryString);
+		parameters.set("wt", "json");
+		
+		QueryResponse response = solr.query(parameters);
+	
+		List<Person> beans =  (List<Person>)response.getBeans(Person.class);
+		
+		System.out.println(beans.size());
+		SolrDocumentList list = response.getResults();
+		
+		return list;
+		
+	}*/
+	
 	
 }
