3d44d70dc2ceee75b33b9c7bba9ce5080c305ad8
SStefanVlad <s.sinescu@levi9.com>
2015-07-24 11:04:14 +0300

    test rest service

--- employee-manager-services/src/test/java/org/employeemanager/services/rest/EmployeeRestServiceTest.java
+++ employee-manager-services/src/test/java/org/employeemanager/services/rest/EmployeeRestServiceTest.java
@@ -1,7 +1,7 @@
-package org.employee_manager.services.rest;
+package org.employeemanager.services.rest;
 
+import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.ClientResponseContext;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
@@ -10,16 +10,13 @@ import javax.ws.rs.core.Response;
 import org.employee_manager.model.Employee;
 import org.junit.Test;
 
-import javax.ws.rs.client.Client;
-
 import junit.framework.Assert;
 
-public class EmployeeTest {
-
+public class EmployeeRestServiceTest {
 	@Test
 	public void testGetEmployee() {
 		Client client = ClientBuilder.newClient();
-		WebTarget target = client.target("http://localhost:8080/employee-manager-web/rest/employee/10");
+		WebTarget target = client.target("http://localhost:8080/employee-manager-container/rest/employee/10");
 		Response response = target.request(MediaType.APPLICATION_JSON).get();
 		System.out.println(response.toString());
 		Assert.assertEquals(200, response.getStatus());
@@ -28,7 +25,7 @@ public class EmployeeTest {
 	@Test
 	public void testGetAllEmployees() {
 		Client client = ClientBuilder.newClient();
-		WebTarget target = client.target("http://localhost:8080/employee-manager-web/rest/employee");
+		WebTarget target = client.target("http://localhost:8080/employee-manager-container/rest/employee");
 		Response response = target.request(MediaType.APPLICATION_JSON).get();
 		System.out.println(response.toString());
 		Assert.assertEquals(200, response.getStatus());
@@ -39,11 +36,11 @@ public class EmployeeTest {
 		Employee emp = new Employee();
 		emp.setName("Gigi");
 		Client client = ClientBuilder.newClient();
-		WebTarget target = client.target("http://localhost:8080/employee-manager-web/rest/employee/");
+		WebTarget target = client.target("http://localhost:8080/employee-manager-container/rest/employee/");
 		Response response = target.request(MediaType.APPLICATION_JSON)
 				.post(Entity.entity(emp, MediaType.APPLICATION_JSON_TYPE));
+		System.out.println(response.toString());
 		Assert.assertEquals(200, response.getStatus());
 	}
 
-	
 }
