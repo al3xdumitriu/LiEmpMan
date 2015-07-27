3b0a6f0adf5503d9a5d69254e3e32eaad9e05b2b
Daniel Pancu <d.pancu@levi9.com>
2015-07-27 09:45:57 +0300

    Rest services

--- employee-manager-services/src/main/java/org/employee_manager/services/repositories/AccountRepository.java
+++ employee-manager-services/src/main/java/org/employee_manager/services/repositories/AccountRepository.java
@@ -2,9 +2,14 @@ package org.employee_manager.services.repositories;
 
 import org.employee_manager.model.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
+
+	@Query("select a from Account a where a.username=:username")
+	Account findAccountByName(@Param("username") String username);
 
 }
