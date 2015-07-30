fc788ae4f7773a7cc794f69589bbb6e365c388be
adelababinciuc <a.babinciuc@levi9.com>
2015-07-30 16:02:09 +0300

    not done

--- employee-manager-web/src/main/webapp/js/controllers.js
+++ employee-manager-web/src/main/webapp/js/controllers.js
@@ -1,53 +1,37 @@
-var raControllers = angular.module('raControllers', []);
-
-raControllers.controller('EmployeesListController', [ '$scope',
-		'EmployeesService', function($scope, EmployeesService)
-
-		{
-
-			$scope.employees = EmployeesService.employees();
-
-		} ]);
-
-raControllers.controller('EmployeeDetailsController', [ '$scope',
-		'$routeParams', 'EmployeesService',
-
-		function($scope, $routeParams, EmployeesService) {
-			$scope.employee = EmployeesService.employee({
-				id : $routeParams.id
-			});
-
-		} ]);
-
-raControllers.controller('AccountController', [ '$scope', '$http', function($scope, $http) {
+var accountApp = angular.module('accountApp', []);
+	accountApp.controller('AccountController', [ '$scope', '$http', '$window', function($scope, $http, $window) {
+		
+		$scope.submissionSuccess = false;
+		
+		$scope.submission = function() {
+		    $scope.submissionSuccess = !$scope.submissionSuccess;
+		   }
+		
+		this.addAccount = function(account){
+			$http({
+				method : 'POST',
+				url : '/employee-manager-container/rest/account',
+				data : account
+			}).success(function (data) {
+				$window.location.href="http://localhost:8080/employee-manager-web/index.jsp#/";
+			  });
+			$scope.submission();
+		};
+	}]);
 	
-	this.addAccount = function(account){
-		$http({
-			method : 'POST',
-			url : '/employee-manager-container/rest/account',
-			data : account
-		});
-	};
+	accountApp.controller('validateCtrl', function($scope) {
+	});
 	
-	var compareTo = function() {
-	    return {
-	        require: "ngModel",
-	        scope: {
-	            otherModelValue: "=compareTo"
-	        },
-	        link: function(scope, element, attributes, ngModel) {
-	             
-	            ngModel.$validators.compareTo = function(modelValue) {
-	                return modelValue == scope.otherModelValue;
-	            };
-	 
-	            scope.$watch("otherModelValue", function() {
-	                ngModel.$validate();
-	            });
-	        }
-	    };
-	};
-	
-	accountApp.directive("compareTo", compareTo);
-	
-}]);
+	accountApp.directive('validationCheck', ['$parse', function ($parse) {
+		  return {
+			    require: 'ngModel',
+			    link: function (scope, elm, attrs, ngModel) {
+			      var check = $parse(attrs.validationCheck);
+
+			      // Watch for changes to this input
+			      scope.$watch(check, function (newValue) {
+			        ngModel.$setValidity(attrs.name, newValue);
+			      });
+			    }
+			  };
+			}]);
